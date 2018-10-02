ffd48008905f39c7f9f96f2e0737d05a3549cc1e
lia <user@es302n07>
2018-10-02 11:15:23 +0800

    new variables

---
+++ exam/src/com/fju/Hello.java
@@ -0,0 +1,16 @@
+package com.fju;
+
+public class Hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        int age = 19;
+        System.out.println(age);
+        float weight = 46.5f;
+        System.out.println(weight);
+        double height = 160.8;
+        System.out.println(height);
+        		
+	}
+
+}
