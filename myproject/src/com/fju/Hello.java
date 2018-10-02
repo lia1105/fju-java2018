26c1aeebd8c74f859a23ef213d7fe4d5b03fbeb8
user <user@es302n07>
2018-10-02 11:10:51 +0800

    variable

--- myproject/src/com/fju/Hello.java
+++ myproject/src/com/fju/Hello.java
@@ -5,6 +5,13 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World");
+        int age = 19;
+        System.out.println(age);
+        float weight = 46.5f;
+        System.out.println(weight);
+        double height = 160.8;
+        System.out.println(height);
+        		
 	}
 
 }
