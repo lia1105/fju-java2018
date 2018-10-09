4494b78c14f39e837335fe4522eeac6275ad6961
user <user@es302n11>
2018-10-09 12:01:20 +0800

    10/9

--- myproject/src/com/fju/Hello.java
+++ myproject/src/com/fju/Hello.java
@@ -4,14 +4,24 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World");
-        int age = 19;
-        System.out.println(age);
-        float weight = 46.5f;
-        System.out.println(weight);
-        double height = 160.8;
-        System.out.println(height);
-        		
+		System.out.println("Hello World");
+
+		Person p = new Person(46.5f, 1.6f);
+		p.weight = 46.5f;
+		p.height = 1.6f;
+
+		System.out.println(p.bmi());
+		p.hello();
+		
+		// Person hank = new Person();
+
+		/*int age = 19;
+		 System.out.println(age);
+		 float weight = 46.5f;
+		 System.out.println(weight);
+		 double height = 1.6;
+		 System.out.println(height);*/
+
 	}
 
 }
