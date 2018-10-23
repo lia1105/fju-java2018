49835727e4cd73d5ab90ddc833030078afe678cb
user <user@es302n23>
2018-10-23 11:25:10 +0800

    RangeTester

---
+++ myproject/src/com/fju/RangeTester.java
@@ -0,0 +1,20 @@
+package com.fju;
+
+import java.util.Scanner;
+
+public class RangeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Please input a number");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		
+		System.out.println("*"+line+"*");
+		boolean number = line.equals(">=-3 && <=5");
+		System.out.println(number);
+		
+		
+	}
+
+}
